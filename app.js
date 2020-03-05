eaf36211f071b0c7213bdfb6849d661b752835d9
Alexander Jansson <Alexander_jansson_91@hotmail.com>
2020-03-05 10:50:58 +0100

    First Upload
    First Upload

---
+++ app.js
@@ -0,0 +1,60 @@
+
+document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelector(".menu").classList.toggle("show");
+});
+
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modalTwo = document.querySelector(button.dataset.modalTarget)
+    openModal(modalTwo)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function openModaltwo(modalTwo) {
+  if (modalTwo == null) return
+  modalTwo.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+
+
+
